77534fedaac4119b23201c092b17148b5ffe4453
윤혜민 <yoonhyemin@yunhyemin-ui-MacBook-Pro.local>
2017-11-22 23:46:11 +0900

    수 찾기

--- baekJoon_1920.java
+++ baekJoon_1920.java
@@ -1,5 +1,5 @@
 /*
- * 수 찾
+ * 수 찾기 
  * https://www.acmicpc.net/problem/1920
  */
 import java.util.Scanner;
@@ -9,6 +9,7 @@ public class baekJoon_1920 {
 	static int[] basicNumbers = new int[20];
 	static int inputNumberOfSearchNumber;
 	static int[] searchNumbers = new int[20];
+	static  int j;
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
@@ -27,11 +28,14 @@ public class baekJoon_1920 {
 
 	private static void baekJoon_1920() {
 		for ( int i = 0; i < inputNumberOfSearchNumber; i++ ) {
-			for ( int j = 0; j < inputNumberOfBasicNumber; j++ ) {
-				if ( searchNumbers[i] == basicNumbers[j] )
+			for ( j = 0; j < inputNumberOfBasicNumber; j++ ) {
+				if ( searchNumbers[i] == basicNumbers[j] ) {
 					System.out.println("1");
+					break;
+				}
 			}
-			//System.out.println("0");
+			if( searchNumbers[i] != basicNumbers[j] )
+				System.out.println("0");
 		}
 	}
 }
